3abf7309a67b635e118d2b1a1c2f260577119091
wayne8jhon <unun1535@hotmail.com>
2016-10-21 16:34:20 +0000

    send01

---
+++ style.js
@@ -0,0 +1,65 @@
+		$(document).ready(function() {
+
+			
+			$("#body01").css({	"font-family": "Arial", 
+								"font-family": "Helvetica",
+								"font-family": "sans-serif",
+								"font-size": "1.125em",
+								"margin": "20px"
+							});
+			
+			$("#h1_1").css({	"text-align": "center",
+								"border-bottom": "2px solid #009",
+								"margin-bottom": "50px",
+			 				});
+			
+			
+			
+			$("#fieldset_1").css( {
+                        		"margin": "1.5em 0 0 0",
+                        		"padding": "0",
+                        		"border": "1px solid #CCC",
+                        	});
+        	
+        	$("#legend_1").css( {
+                        		"margin-left": "1em",
+                        		"color": "#009",
+                        		"font-weight": "bold",
+                        	});
+        	
+        	$("#label_1").css( {
+                        		"float": "left",
+                        		"width": "10em",
+                        		"margin-right": "1em",
+                        	});
+        	
+        	
+        	$("#fieldset_1").$("#ol_1").css( {
+        		"list-style": "none",
+        		"padding-top": "3px",
+        		"padding-left": "2em",
+        		"padding-bottom": "3px",
+        	});
+        	
+        // 	$("#fieldset_1").$("#li_1").css( {
+        // 		"line-height": "24px",
+        // 		"margin-top": "5px",
+        // 		"margin-bottom": "5px",
+        // 	});
+        	
+        // 	$("#fieldset_1").$("#li_1").$("input_1").css( {
+        // 		"line-height": "24px",
+        // 		"height": "24px",
+        // 		"border": "1px solid #CCC",
+        // 	});
+        	
+        // 	$("#btnSubmit").css( {
+        // 		"border-style": "none",
+        // 		"width": "10em",
+        // 		"height": "3em",
+        // 		"margin": "-1em auto 1em 18em",
+        // 	});
+			
+			
+		
+		});
